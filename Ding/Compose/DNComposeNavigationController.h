5056bc6c39ca43e4e845cf61a3fde9902cdd065d
Mark Miyashita <negativetwelve@gmail.com>
2014-04-05 14:04:48 -0700

    Add DNComposeNavigationController

---
+++ Ding/Compose/DNComposeNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  DNComposeNavigationController.h
+//  Ding
+//
+//  Created by Mark Miyashita on 4/5/14.
+//  Copyright (c) 2014 Mark Miyashita. All rights reserved.
+//
+
+#import "DNNavigationController.h"
+
+@interface DNComposeNavigationController : DNNavigationController
+
+@end
