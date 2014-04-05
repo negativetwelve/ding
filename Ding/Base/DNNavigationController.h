4e781092bb5094e8ab06e6c77321206dc2302733
Mark Miyashita <negativetwelve@gmail.com>
2014-04-05 13:43:08 -0700

    Add base view controllers

---
+++ Ding/Base/DNNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  DNNavigationController.h
+//  Ding
+//
+//  Created by Mark Miyashita on 4/5/14.
+//  Copyright (c) 2014 Mark Miyashita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DNNavigationController : UINavigationController
+
+@end
