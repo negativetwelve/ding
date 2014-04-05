e3f41db5971ea3882c40193dd3ff0f45b6d5447e
Mark Miyashita <negativetwelve@gmail.com>
2014-04-05 14:39:10 -0700

    Add default home view controller

---
+++ Ding/Home/DNHomeViewController.m
@@ -0,0 +1,38 @@
+//
+//  DNHomeViewController.m
+//  Ding
+//
+//  Created by Mark Miyashita on 4/5/14.
+//  Copyright (c) 2014 Mark Miyashita. All rights reserved.
+//
+
+#import "DNHomeViewController.h"
+
+@interface DNHomeViewController ()
+
+@end
+
+@implementation DNHomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
