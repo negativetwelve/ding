3514267d855824585220ebbdcfa3a0195bcf2197
Mark Miyashita <negativetwelve@gmail.com>
2014-04-05 14:03:38 -0700

    Add DNLoginNavigationController

---
+++ Ding/Login/DNLoginNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  DNLoginNavigationController.h
+//  Ding
+//
+//  Created by Mark Miyashita on 4/5/14.
+//  Copyright (c) 2014 Mark Miyashita. All rights reserved.
+//
+
+#import "DNNavigationController.h"
+
+@interface DNLoginNavigationController : DNNavigationController
+
+@end
