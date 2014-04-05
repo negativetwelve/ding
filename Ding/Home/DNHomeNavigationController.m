e79c18185793425eb4869a13672aad95acbc77b5
melhuang <melissahuang10@gmail.com>
2014-04-05 14:05:04 -0700

    home nav cont

---
+++ Ding/Home/DNHomeNavigationController.m
@@ -0,0 +1,38 @@
+//
+//  DNHomeNavigationController.m
+//  Ding
+//
+//  Created by Melissa on 4/5/14.
+//  Copyright (c) 2014 Mark Miyashita. All rights reserved.
+//
+
+#import "DNHomeNavigationController.h"
+
+@interface DNHomeNavigationController ()
+
+@end
+
+@implementation DNHomeNavigationController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
