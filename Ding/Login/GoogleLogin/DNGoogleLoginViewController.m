0f7d8cb087a940fcd7c0218f40ad245c09967d00
Mark Miyashita <negativetwelve@gmail.com>
2014-04-05 17:56:29 -0700

    Make text fields objects and store with core data

--- Ding/Login/GoogleLogin/DNGoogleLoginViewController.m
+++ Ding/Login/GoogleLogin/DNGoogleLoginViewController.m
@@ -13,6 +13,8 @@
 @end
 
 @implementation DNGoogleLoginViewController
+@synthesize emailTextField;
+@synthesize passwordTextField;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -28,34 +30,56 @@
     [self setLoginFields];
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+
+    emailTextField.text = [[NSUserDefaults standardUserDefaults] stringForKey:kXMPPmyGoogleJID];
+    passwordTextField.text = [[NSUserDefaults standardUserDefaults] stringForKey:kXMPPmyGooglePassword];
+}
+
 - (void)submitAction: (id)selector {
     NSLog(@"submit action called");
+    [self setField:emailTextField forKey:kXMPPmyGoogleJID];
+    [self setField:passwordTextField forKey:kXMPPmyGooglePassword];
+    
+    [self dismissViewControllerAnimated:YES completion:NULL];
 }
 
 - (void)setLoginFields {
-    UITextField *emailTextField = [[UITextField alloc] initWithFrame:CGRectMake(20, 140, 280, 31)];
-    [emailTextField setPlaceholder:@"Email"];
-    [emailTextField setReturnKeyType:UIReturnKeyNext];
-    [emailTextField setTag:1];
-    [emailTextField setDelegate:self];
-    [emailTextField setKeyboardType:UIKeyboardTypeEmailAddress];
-    [emailTextField setAutocapitalizationType:UITextAutocapitalizationTypeNone];
-    [self.view addSubview:emailTextField];
+    UITextField *emailField = [[UITextField alloc] initWithFrame:CGRectMake(20, 140, 280, 31)];
+    [emailField setPlaceholder:@"Email"];
+    [emailField setReturnKeyType:UIReturnKeyNext];
+    [emailField setTag:1];
+    [emailField setDelegate:self];
+    [emailField setKeyboardType:UIKeyboardTypeEmailAddress];
+    [emailField setAutocapitalizationType:UITextAutocapitalizationTypeNone];
+    [self.view addSubview:emailField];
     
-    UITextField *passwordTextField = [[UITextField alloc] initWithFrame:CGRectMake(20, 180, 280, 31)];
-    [passwordTextField setPlaceholder:@"Password"];
-    [passwordTextField setReturnKeyType:UIReturnKeyDone];
-    [passwordTextField setTag:2];
-    [passwordTextField setDelegate:self];
-    [passwordTextField setSecureTextEntry:YES];
-    [passwordTextField addTarget:self action:@selector(submitAction:) forControlEvents:UIControlEventEditingDidEndOnExit];
-    [self.view addSubview:passwordTextField];
+    UITextField *passwordField = [[UITextField alloc] initWithFrame:CGRectMake(20, 180, 280, 31)];
+    [passwordField setPlaceholder:@"Password"];
+    [passwordField setReturnKeyType:UIReturnKeyDone];
+    [passwordField setTag:2];
+    [passwordField setDelegate:self];
+    [passwordField setSecureTextEntry:YES];
+    [passwordField addTarget:self action:@selector(submitAction:) forControlEvents:UIControlEventEditingDidEndOnExit];
+    [self.view addSubview:passwordField];
     
     UIButton *submitButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     [submitButton setTitle:@"Submit" forState:UIControlStateNormal];
     [submitButton setFrame:CGRectMake(70, 220, 180, 50)];
     [submitButton addTarget:self action:@selector(submitAction:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:submitButton];
+    
+    [self setEmailTextField:emailField];
+    [self setPasswordTextField:passwordField];
+}
+
+- (void)setField:(UITextField *)field forKey:(NSString *)key {
+    if (field.text != nil) {
+        [[NSUserDefaults standardUserDefaults] setObject:field.text forKey:key];
+    } else {
+        [[NSUserDefaults standardUserDefaults] removeObjectForKey:key];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
